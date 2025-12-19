f47f814eea052f25f8f7d6d331bd50dba21b549e
s1cklove <roman.s.zinin@gmail.com>
2025-12-19 21:47:36 +0300

    Add HP logic

--- Assets/Scripts/Gameplay/Map/Star.cs
+++ Assets/Scripts/Gameplay/Map/Star.cs
@@ -2,34 +2,70 @@ using UnityEngine;
 
 namespace Gameplay.Map
 {
+    public enum StarState
+    {
+        White,  // Neutral
+        Blue,   // Controlled by Blue player
+        Red     // Controlled by Red player
+    }
+
+    public enum Player
+    {
+        Blue,
+        Red
+    }
+
     public class Star : MonoBehaviour
     {
+        [Header("Identification")]
+        [SerializeField] private int _id;
+        [SerializeField] private int _constellationId = -1; // -1 if not part of a constellation
+
         [Header("Star Data")]
         [SerializeField] private Vector2 _coordinates;
-        [SerializeField] private int _owner = -1; // -1 for no owner
         [SerializeField] private int _size; // 1-5
-        [SerializeField] private float _value;
+        [SerializeField] private int _hp; // 1-20
 
-        private bool _hasOwner => _owner != -1;
+        [Header("Combat State")]
+        [SerializeField] private int _blueDamage;
+        [SerializeField] private int _redDamage;
 
         // Getters
+        public int Id => _id;
+        public int ConstellationId => _constellationId;
         public Vector2 Coordinates => _coordinates;
-        public int Owner => _owner;
-        public bool HasOwner => _hasOwner;
         public int Size => _size;
-        public float Value => _value;
+        public int HP => _hp;
+        public int BlueDamage => _blueDamage;
+        public int RedDamage => _redDamage;
+
+        public StarState State
+        {
+            get
+            {
+                if (_blueDamage >= _hp)
+                    return StarState.Blue;
+                if (_redDamage >= _hp)
+                    return StarState.Red;
+                return StarState.White;
+            }
+        }
 
         // Setters
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public void SetConstellationId(int constellationId)
+        {
+            _constellationId = constellationId;
+        }
+
         public void SetCoordinates(Vector2 coordinates)
         {
             _coordinates = coordinates;
             transform.position = new Vector3(coordinates.x, coordinates.y, 0);
-        }
-
-        public void SetOwner(int ownerId)
-        {
-            _owner = ownerId;
-            UpdateVisuals();
         }
 
         public void SetSize(int size)
@@ -38,32 +74,61 @@ namespace Gameplay.Map
             UpdateScale();
         }
 
-        public void SetValue(float value)
+        public void SetHP(int hp)
         {
-            _value = value;
+            _hp = Mathf.Clamp(hp, 1, 20);
         }
 
-        public void Initialize(Vector2 coordinates, int size, float initialValue)
+        public void Initialize(int id, Vector2 coordinates, int size, int hp)
         {
+            SetId(id);
             SetCoordinates(coordinates);
             SetSize(size);
-            SetValue(initialValue);
-            _owner = -1;
+            SetHP(hp);
+            _blueDamage = 0;
+            _redDamage = 0;
+            _constellationId = -1;
         }
 
-        public void ChangeValue(float amount)
+        /// <summary>
+        /// Apply damage to the star from a specific player.
+        /// If the enemy has accumulated damage, it decreases their counter instead.
+        /// </summary>
+        public void ApplyDamage(Player player, int amount)
         {
-            _value += amount;
-            if (_value <= 0 && _hasOwner)
+            if (amount <= 0) return;
+
+            if (player == Player.Blue)
             {
-                // Star loses ownership when value drops to 0 or below
-                SetOwner(-1);
+                // If Red has damage, decrease it first
+                if (_redDamage > 0)
+                {
+                    _redDamage = Mathf.Max(0, _redDamage - amount);
+                }
+                else
+                {
+                    _blueDamage += amount;
+                }
             }
+            else // Player.Red
+            {
+                // If Blue has damage, decrease it first
+                if (_blueDamage > 0)
+                {
+                    _blueDamage = Mathf.Max(0, _blueDamage - amount);
+                }
+                else
+                {
+                    _redDamage += amount;
+                }
+            }
+
+            UpdateVisuals();
         }
 
         private void UpdateVisuals()
         {
-            // TODO: Update visual color based on owner
+            // TODO: Update visual color based on State
             // This will be implemented when we add visual feedback
         }
 
